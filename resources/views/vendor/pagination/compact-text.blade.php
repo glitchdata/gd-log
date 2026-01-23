63d119106acf00e3421b2ae79aad5a13a5460054
T Rock <terence.chia@gmail.com>
2026-01-23 12:00:37 +1100

    page+++

--- resources/views/vendor/pagination/compact-text.blade.php
+++ resources/views/vendor/pagination/compact-text.blade.php
@@ -1,8 +1,6 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Pagination Navigation" class="pagination-text">
-        <div class="pagination-text__summary">
-            <span>Page {{ $paginator->currentPage() }} of {{ $paginator->lastPage() }}</span>
-        </div>
+        <div class="pagination-text__summary">Page {{ $paginator->currentPage() }} of {{ $paginator->lastPage() }}</div>
         <div class="pagination-text__links">
             @if ($paginator->onFirstPage())
                 <span class="pagination-text__disabled" aria-disabled="true">Previous</span>
